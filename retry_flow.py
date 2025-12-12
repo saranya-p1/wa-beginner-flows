eef1a75b18385bba0beefe4d3610954527cc71e2
saranya-p1 <saranyapalani2803@gmail.com>
2025-12-12 12:56:52 +0530

    Update retry_flow.py

--- retry_flow.py
+++ retry_flow.py
@@ -4,12 +4,12 @@ from random import randint
 
 @task(retries=3, retry_delay_seconds=2)
 def unstable_task():
-n = randint(1, 10)
-if n <= 7:
-raise Exception("Random failure!")
-print("Success on attempt")
+  n = randint(1, 10)
+  if n <= 7:
+    raise Exception("Random failure!")
+    print("Success on attempt")
 
 @flow
 def retry_example():
-unstable_task()
+  unstable_task()
 
